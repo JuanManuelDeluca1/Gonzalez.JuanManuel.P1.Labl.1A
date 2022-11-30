27aae323f2024c569e1a5e5a2319691851893298
JuanManuelDeluca1 <101626028+JuanManuelDeluca1@users.noreply.github.com>
2022-11-30 10:24:15 -0300

    Add files via upload

---
+++ Parcial 2 movies/src/Parser.c
@@ -0,0 +1,45 @@
+/*
+ * Parser.c
+ *
+ *  Created on: 23 nov. 2022
+ *      Author: juanm
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include "parser.h"
+#include "LinkedList.h"
+#include "movies.h"
+
+int parser_MoviesFromText(FILE* pFile , LinkedList* pArrayListMovies)
+{
+	int todoOk = -1;
+	char id[50];
+	char nombre[50];
+	char genero[50];
+	char tiempo[30];
+	char encabezado[3][20];
+	eMovies* auxJugador;
+	if(pArrayListMovies != NULL && pFile != NULL)
+	{
+		todoOk = 0;
+		fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n", encabezado[0], encabezado[1], encabezado[2], encabezado[3]);
+		while(!feof(pFile))
+		{
+			fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n", id, nombre, genero, tiempo);
+			auxJugador = mov_newParametros(id, nombre, genero, tiempo);
+			if(auxJugador != NULL)
+			{
+				if(ll_add(pArrayListMovies, auxJugador) == 0)
+				{
+					todoOk = 1;
+				}
+			}
+		}
+
+	}
+	fclose(pFile);
+    return todoOk;
+}
